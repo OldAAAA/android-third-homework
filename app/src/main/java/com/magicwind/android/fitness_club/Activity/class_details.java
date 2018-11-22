fc2a9491d257c20d63bc597d15d07e53b4813bf1
OldAAAA <1074201527@qq.com>
2018-11-22 19:13:32 +0800

    播放视频功能

--- app/src/main/java/com/magicwind/android/fitness_club/Activity/class_details.java
+++ app/src/main/java/com/magicwind/android/fitness_club/Activity/class_details.java
@@ -1,16 +1,173 @@
 package com.magicwind.android.fitness_club.Activity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.MediaController;
 
 import com.magicwind.android.fitness_club.R;
 
-public class class_details extends AppCompatActivity {
+import java.io.IOException;
+
+public class class_details extends AppCompatActivity implements
+        MediaController.MediaPlayerControl,
+        MediaPlayer.OnBufferingUpdateListener,
+        SurfaceHolder.Callback{
+
+    //媒体播放器
+    private MediaPlayer mediaPlayer;
+    //视频控制器
+    private MediaController controller;
+    //缓存
+    private int bufferPercentage = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         getSupportActionBar().hide();
+        mediaPlayer = new MediaPlayer();
+        controller = new MediaController(this);
+        controller.setAnchorView(findViewById(R.id.vedio));
+        initSurfaceView();
+    }
+
+    private void initSurfaceView() {
+        SurfaceView videoSuf = (SurfaceView) findViewById(R.id.surfaceView);
+        videoSuf.setZOrderOnTop(false);
+        videoSuf.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        videoSuf.getHolder().addCallback(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            String path = Environment.getExternalStorageDirectory().getPath() + "/1.mp4";
+            mediaPlayer.setDataSource(path);
+            mediaPlayer.setOnBufferingUpdateListener(this);
+            //mediaPlayer.prepare();
+
+            controller.setMediaPlayer(this);
+            controller.setEnabled(true);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != mediaPlayer){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        controller.show();
+        return super.onTouchEvent(event);
+    }
+
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        bufferPercentage = percent;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mediaPlayer.setDisplay(holder);
+        mediaPlayer.prepareAsync();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void start() {
+        if (null != mediaPlayer){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (null != mediaPlayer){
+            mediaPlayer.pause();
+        }
+    }
+
+
+
+    @Override
+    public int getDuration() {
+        return mediaPlayer.getDuration();
+    }
+
+    @Override
+    public int getCurrentPosition() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    @Override
+    public void seekTo(int pos) {
+        mediaPlayer.seekTo(pos);
+    }
+
+    @Override
+    public boolean isPlaying() {
+        if (mediaPlayer.isPlaying()){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getBufferPercentage() {
+        return bufferPercentage;
+    }
+
+    @Override
+    public boolean canPause() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekBackward() {
+        return false;
+    }
+
+    @Override
+    public boolean canSeekForward() {
+        return false;
+    }
+
+    @Override
+    public int getAudioSessionId() {
+        return 0;
     }
 }
